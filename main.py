efefbe551f2e5ee78a743a9d56c38970302f83eb
Gustavo Huc <gustavo.huc@gmail.com>
2025-09-11 22:26:39 -0300

    Preco Papel Hig

--- main.py
+++ main.py
@@ -24,3 +24,16 @@ async def preco_ovos(preco: float,qtd: int):
             "Preço por Ovo": cifrao+ str(round(preco_por_ovo,3)),
             "Preço da bandeja": cifrao+ str(round(preco_duzia,3))
     }
+@app.get("/preco_papel_hig/")
+async def preco_papel_hig(preco: float,qtd: int, metros:int):
+
+    preco_pacote = preco
+    preco_metro = preco/(metros * qtd)
+    preco_rolo = preco/qtd
+    cifrao = "R$"
+
+    return {"Preço Total" : cifrao+ str(preco),
+            "Quantidade de rolos:": qtd,
+            "Preço por metro": cifrao+ str(round(preco_metro,3)),
+            "Preço por rolo": cifrao+ str(round(preco_rolo,3))
+    }
